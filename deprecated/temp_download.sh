3ef071204ae8e48c700f777a47141b1a49ca3f53
ray57512 <ray57512@gmail.com>
2020-05-07 16:12:01 -0400

    cleanup

--- deprecated/temp_download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scp cc2553@transfer-grace.hpc.yale.edu:/home/fas/admins/cc2553/scratch60/community_selection_project/data/species_pool_17/*-function.txt /Users/chang-yu/Desktop/Lab/community-selection/data/species_pool_17/
--- deprecated/template_download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scp cc2553@transfer-grace.hpc.yale.edu:/home/fas/admins/cc2553/scratch60/community_selection_project/data/species_pool_arg1/*-function.txt /Users/chang-yu/Desktop/Lab/community-selection/data/species_pool_arg1/
