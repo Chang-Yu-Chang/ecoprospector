681588da7cd5a09e2969b3504430c7253dbabc35
Chang-Yu-Chang <ray57512@gmail.com>
2020-07-28 17:59:39 -0400

    Add extension to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
-
+master_doc = 'index'
 project = 'Ecoprospector'
 copyright = '2020, Chang-Yu Chang'
 author = 'Chang-Yu Chang'
@@ -30,7 +31,9 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['recommonmark',
+              'sphinx_rtd_theme',
+              'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +50,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
