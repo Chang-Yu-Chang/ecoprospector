59f8d5377bc648bad631df485c998e60426cd028
Chang-Yu-Chang <ray57512@gmail.com>
2020-08-05 16:55:11 -0400

    Add example mapping file data/input_test.csv

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ setup(name='ecoprospector',
       version='0.0.1',
       description='Simulate community selection protocols',
       url='https://github.com/Chang-Yu-Chang/ecoprospector',
-      author= ['Chang-Yu Chang', 'Jean Villa'], 
+      author=['Chang-Yu Chang', 'Jean Villa'], 
       author_email=['chang-yu.chang@yale.edu'],
       license='MIT',
       packages = ['community_selection'],
       scripts = ['commandline_tool/ecoprospector'],
+      include_package_data = True,
+      package_data = {"": ["*.csv"]},
       zip_safe=False)
 
